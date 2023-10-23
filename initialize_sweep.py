18b0863c88d80bb1aed01403985e790408752187
Adam-Boesky <apboesky@gmail.com>
2023-10-23 10:52:43 -0400

    Work on submitting sweeps to the cluster

--- initialize_sweep.py
+++ initialize_sweep.py
@@ -1,4 +1,8 @@
 """Grid search to tune hyperparameters"""
+import os
+import sys
+from subprocess import Popen, PIPE, call
+
 import numpy as np
 import wandb
 import torch
@@ -10,19 +14,49 @@ from neural_net import load_and_preprocess, get_model, get_tensor_batch, checkpo
 
 LOG = get_clean_logger(logger_name = Path(__file__).name)
 PROJECT = 'Astronomy 98'
-with open('/Users/adamboesky/Research/ay98/sweep_config.yaml', 'r') as f:
+with open('/n/home04/aboesky/berger/Weird_Galaxies/sweep_config.yaml', 'r') as f:
     SWEEP_CONFIG = yaml.safe_load(f)
-SWEEP_BASH_SCRIPT = """
-#!/bin/bash
+# SWEEP_BASH_SCRIPT = """#!/bin/bash
 
-#SBATCH -c 48                               # Number of cores (-c)
-#SBATCH --job-name=%s                       # This is the name of your job
-#SBATCH --mem=184G                          # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -t 0-12:00                          # Runtime in D-HH:MM, minimum of 10 minutes
+# #SBATCH -c 48                                       # Number of cores (-c)
+# #SBATCH --job-name={job_name}                       # This is the name of your job
+# #SBATCH --mem=184G                                  # Memory pool for all cores (see also --mem-per-cpu)
+# #SBATCH -t 0-12:00                                  # Runtime in D-HH:MM, minimum of 10 minutes
+
+# # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
+# #SBATCH -o cluster_logs/myoutput_\%j.out                          # File to which STDOUT will be written, %j inserts jobid
+# #SBATCH -e cluster_logs/myerrors_\%j.err                          # File to which STDERR will be written, %j inserts jobid
+# #SBATCH --mail-user=aboesky@college.harvard.edu     # Send email to user
+
+# #SBATCH -p shared
+
+# # Remember:
+# # The variable $TMPDIR points to the local hard disks in the computing nodes.
+# # The variable $HOME points to your home directory.
+# # The variable $SLURM_JOBID stores the ID number of your job.
+
+
+# # Load modules
+# #################################
+# module load python/3.10.12-fasrc01
+# conda activate ay98
+
+# # Commands
+# #############################
+# export WANDB_API_KEY=6ecd8ea5ceb5a64219d98bc34ce67af0904f2be8
+# wandb agent {sweep_id}
+# # """
+SWEEP_BASH_SCRIPT = """#!/bin/bash
+
+#SBATCH -c 12                                       # Number of cores (-c)
+#SBATCH --job-name={job_name}                       # This is the name of your job
+#SBATCH --mem=56G                                  # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -t 0-00:10                                  # Runtime in D-HH:MM, minimum of 10 minutes
 
 # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
-#SBATCH -o myoutput_%j.out                  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e myerrors_%j.err                  # File to which STDERR will be written, %j inserts jobid
+#SBATCH -o cluster_logs/myoutput_\%j.out                          # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e cluster_logs/myerrors_\%j.err                          # File to which STDERR will be written, %j inserts jobid
+#SBATCH --mail-user=aboesky@college.harvard.edu     # Send email to user
 
 #SBATCH -p test
 
@@ -31,15 +65,18 @@ SWEEP_BASH_SCRIPT = """
 # The variable $HOME points to your home directory.
 # The variable $SLURM_JOBID stores the ID number of your job.
 
+# Export environment variables
+#################################
+export WANDB_API_KEY=6ecd8ea5ceb5a64219d98bc34ce67af0904f2be8
 
 # Load modules
 #################################
 module load python/3.10.12-fasrc01
-conda activate ay98
+source activate pt2.1.0_cuda12.1
 
 # Commands
 #############################
-wandb agent %s
+wandb agent {sweep_id}
 """
 # {
 #                 'method': 'grid',
@@ -62,7 +99,7 @@ def train(config=None):
     all_cat, all_photo, photo_train, photo_test, cat_train, cat_test, photo_err_train, photo_err_test, cat_err_train, \
             cat_err_test, photo_norm, photo_mean, photo_std, photo_err_norm, cat_norm, cat_mean, cat_std, cat_err_norm = load_and_preprocess()
     
-    with wandb.init(project=PROJECT, config=config):
+    with wandb.init(project=PROJECT, config=config, mode='offline'):
         config = wandb.config
 
         ######################## MAKE NN, LOSS FN, AND OPTIMIZER  ########################
@@ -132,16 +169,37 @@ def train(config=None):
 
 def tune_parameters():
     """Use grid search to tune hyperparameters."""
+    # Set api key
+    os.environ["WANDB_API_KEY"] = '6ecd8ea5ceb5a64219d98bc34ce67af0904f2be8'
+
     # Make sweep and set sweep ID environment variable
+    wandb.init(project=PROJECT)
     sweep_id = wandb.sweep(SWEEP_CONFIG, project=PROJECT)
 
     # Submit a number of agents to complete the sweep
-    for i in range(15):
+    for i in range(2):
         job_name = f'{sweep_id}_agent_{i}'
         sbatchFile = open('submit_agent.sh', 'w')
         LOG.info('Submitting agent %i', i)
-        sbatchFile.write(SWEEP_BASH_SCRIPT % (job_name, sweep_id))
+        sbatchFile.write(SWEEP_BASH_SCRIPT.format(job_name=job_name, sweep_id=sweep_id))
         sbatchFile.close()
+
+        # Open a pipe to the sbatch command.
+        sbatch_command = f'sbatch submit_agent.sh'
+        proc = Popen(sbatch_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+
+        # Send job_string to sbatch
+        if (sys.version_info > (3, 0)):
+            proc.stdin.write(sbatch_command.encode('utf-8'))
+        else:
+            proc.stdin.write(sbatch_command)
+
+        LOG.info('\tsbatch command: %s', sbatch_command)
+        out, err = proc.communicate()
+        LOG.info("\tout = %s", out)
+        job_id = out.split()
+        LOG.info("\tjob_id: %s", job_id)
+        LOG.info("\terror: %s", err)
     # wandb.agent(sweep_id, function=train)
 
 
---
+++ submit_agent.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH -c 12                                       # Number of cores (-c)
+#SBATCH --job-name=nvzbx8hr_agent_1                       # This is the name of your job
+#SBATCH --mem=56G                                  # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -t 0-00:10                                  # Runtime in D-HH:MM, minimum of 10 minutes
+
+# Paths to STDOUT or STDERR files should be absolute or relative to current working directory
+#SBATCH -o cluster_logs/myoutput_\%j.out                          # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e cluster_logs/myerrors_\%j.err                          # File to which STDERR will be written, %j inserts jobid
+#SBATCH --mail-user=aboesky@college.harvard.edu     # Send email to user
+
+#SBATCH -p test
+
+# Remember:
+# The variable $TMPDIR points to the local hard disks in the computing nodes.
+# The variable $HOME points to your home directory.
+# The variable $SLURM_JOBID stores the ID number of your job.
+
+# Export environment variables
+#################################
+export WANDB_API_KEY=6ecd8ea5ceb5a64219d98bc34ce67af0904f2be8
+
+# Load modules
+#################################
+module load python/3.10.12-fasrc01
+source activate pt2.1.0_cuda12.1
+
+# Commands
+#############################
+wandb agent nvzbx8hr
